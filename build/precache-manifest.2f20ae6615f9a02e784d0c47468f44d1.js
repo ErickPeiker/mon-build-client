51e555449d5521cb3de75797a94cb4f5238b77e6
erickpeiker-olwmo <erick@owlmo.com.br>
2020-03-02 22:35:23 -0300

    Projeto para buildar a aplicação client para web

---
+++ build/precache-manifest.2f20ae6615f9a02e784d0c47468f44d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6a2b8a395d39e025d2ccd4157b41fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e0b1e9c41cb01b17601",
+    "url": "/static/js/2.34f9fad3.chunk.js"
+  },
+  {
+    "revision": "9fe3ba9e9d249786271c",
+    "url": "/static/js/main.7a8a659f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/precache-manifest.a44b652a8fd4ef7e935fa3d858ebabca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180b2582ea6cdd68b76fd65d341db4ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20840ca79ebdbb34861c",
+    "url": "/static/js/2.d5f4b016.chunk.js"
+  },
+  {
+    "revision": "ff7982453d29af265d01",
+    "url": "/static/js/main.1f0d21d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
